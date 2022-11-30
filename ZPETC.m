04e4b53a22766a28f652fdefecb6aa66107dae51
Edward Bryson <edbryson7@gmail.com>
2022-11-30 12:08:31 -0500

    Velocity Domain working ZPETC

--- ZPETC.m
+++ ZPETC.m
@@ -18,37 +18,45 @@ TF_s=tf([num],[den]); % plant TF in s-domain - Theta/v
 s = tf('s');
 G = 1/s + TF_s;
 
-% G = G*s;
+G_s = G;
+G = G*s;
 
 ts = 0.051;
-TF_z = c2d(G, ts, 'tustin');
+TF_z = c2d(G, ts)
 
 %% Z Domain Controller
 z = tf('z', ts);
 
 s = 3;
-C_z = (1/0.0002613) * ((z^3 - 2.98*z^2 + 2.974*z - 0.9939) * ((z^-1) + 1.7783) * z^-s) / ((z-0.0848)*(1+1.7783)^2);
+% C_z = (1/0.0002613) * ((z^3 - 2.98*z^2 + 2.974*z - 0.9939) * ((z^-1) + 1.7783) * z^-s) / ((z-0.0848)*(1+1.7783)^2);
 % C_z = (1/0.0002613) * ((z^3 - 2.98*z^2 + 2.974*z - 0.9939) * ((z^-1) + 0.5623) * z^-s) / ((z-0.0848)*(1+1.7783)^2);]
 
 s=1;
-% % C_z = (1/0.0001745) * ((z^3 - 2.979*z^2 + 2.973*z - 0.9936) * ((z^-1) + 1.0177) * z^-s) / ((z+0.0702022)*(z+0.98257)*(1+1.0177)^2);
+% C_z = (1/0.0001745) * ((z^3 - 2.979*z^2 + 2.973*z - 0.9936) * ((z^-1) + 1.0177) * z^-s) / ((z+0.0702022)*(z+0.98257)*(1+1.0177)^2);
 
 % s=4;
 % C_z = (1/0.006844) * (z^3 - 2.979*z^2 + 2.973*z - 0.9936) * (z-1) * (z+1) * z^-s / ((z+0.070275)*4);
 
-% C_z = C_z / 1.7;
 
-G_z = TF_z * C_z;
+% TF_z= (0.0002613*(1/z) + 0.0004425*(1/z^2) - (3.942*10^-5)*(1/z^3)) / (1 - 2.98*(z^-1) + 2.974*(z^-2) - 0.9939*(z^-3))
+
+% C_z = ((1/0.0002879)*(z^3 - 2.979*z^2 + 2.973*z - 0.9939) * ((z^-1)+1.8052)*z^-s) / ( (z-0.0727)*(1+1.8052)^2)
+
+% C_z = 4.4248*(1-2.979*z^-1 + 2.973*z^-2 - 0.9939*z^-3) * z^-1 / (-0.0000378 * ((z^-1)-17.385) * ((z^-1)+4.34269));
+
+% TF_z= (0.0002879*z^-1 + 0.0004988*z^-2 - (3.78*10^-5)*z^-3) / (1 - 2.979*(z^-1) + 2.973*(z^-2) - 0.9939*(z^-3))
+
+C_z = inv(TF_z)*inv(z)
 
 %% Smooth trajectory generation
 
 % yd = sin(t)/5;
 
 y0 = 0;
-yf = .25;
+yf = 0.7;
 
-tf = yf/0.1;
-% tf = 15;
+% tf = yf/0.1;
+tf = 20;
 
 t = [0:ts:tf];
 
@@ -62,15 +70,20 @@ a3 = -2/(tf^3)*(yf-y0);
 
 yd = a0 + a1*t + a2*t.^2 + a3*t.^3;
 
-% yd = t/50;
+yd = t/30;
 % yd(150:end) = yd(150);
 
+pos = yd;
 vel = diff(yd)/ts;
 accel =diff(diff(yd))/ts;
 
-% yd = [vel vel(end)];
+yd = [vel vel(end)];
 
 yd = yd_s(yd, s);
+
+% scale = 1.8*(2/pi)*atan(2.7*yf*pi/2);
+scale=1;
+% C_z = C_z/scale;
 
 
 % yd = 0 * yd;
@@ -82,9 +95,10 @@ r_y = lsim(C_z, yd, t);
 y_max = 0.2;
 y_min = -0.2;
 r_y = actuator_limit(r_y, y_min, y_max);
-% r_y = r_y/1.7;
 
-y = lsim(G, r_y, t);
+r_y = r_y/scale;
+
+y = lsim(G_s, r_y, t);
 
 figure()
 plot(t, yd);
@@ -115,7 +129,9 @@ title('Simulated Plant Output y')
 xlabel('time (s)')
 ylabel('m')
 
-out = lsim(G_z, yd, t);
+
+G_z = TF_z * C_z;
+out = lsim(G_s, yd, t);
 figure()
 hold on
 plot(t, yd)
@@ -125,10 +141,10 @@ xlabel('t (s)')
 ylabel('m')
 legend(["y_d", "y"])
 
-disp(sprintf('yd: %d\ntf: %d\n perc overshoot: %.3f', yf, tf, y(end)/yf));
+fprintf('%.3f, %.3f\n', yf, y(end)/yf);
 
 %%
-% writematrix(round(100*r_y/0.2), 'r_y.csv');
+writematrix(round(100*r_y/0.2), 'r_y.csv');
 
 %%
 function u = actuator_limit(u, min, max)
--- plotting.m
+++ plotting.m
@@ -1,23 +1,10 @@
 clc;close all;%clear;
 
-pause=readtable('GT_BridgeCrane_Data_with pause.csv');
-nopause=readtable('GT_BridgeCrane_Data_without pause.csv');
+nopause=readtable('GT_BridgeCrane_Data.csv');
+    
+nopause(492:end,:)=[];
 
-pause(281:end,:)=[];
-nopause(223:end,:)=[];
-
-headers=pause.Properties.VariableNames';
-
-figure()
-subplot(3,1,1)
-plot(pause.TimeY_dir_sec_,pause.YActualVelocity_mm_sec_/1000);
-
-subplot (3,1,2)
-plot(pause.TimeY_dir_sec_,((pause.YCranePosition_mm_-pause.YCranePosition_mm_(1))+pause.YPayloadDeflection_mm_)/1000);
-
-subplot(3,1,3)
-plot(pause.TimeY_dir_sec_,pause.YPayloadDeflection_mm_/1000);
-
+headers=nopause.Properties.VariableNames';
 
 
 figure()
